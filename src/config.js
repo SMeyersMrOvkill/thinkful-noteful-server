5bf0360cd3288952c2e704037ed8a5e8c931e24e
Samuel Meyers <thecheesecakejunkie@gmail.com>
2020-12-09 13:44:49 -0600

    Added stuff

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: process.env.PORT || 8080,
+    PORT: process.env.PORT || 9090,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgres://muffin:password@localhost/noteful'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://muffin:password@localhost/noteful'
 }
